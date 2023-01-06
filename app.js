af4a943ebded4d6e2b8114a15d49822c556943d0
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-01-05 22:16:12 -0300

    05.01

---
+++ app.js
@@ -0,0 +1,13 @@
+const titles = document.querySelector(".title-container");
+
+const rows = 6;
+const columns = 5;
+
+for(let i; i < rows; i++) {
+    const titleRow = document.createElement("div");
+    titleRow.setAttribute("id", rowIndex);
+    titleRow.setAttribute("class", title-row);
+    for(let j = 0; j < columns; j++) {
+        const element = array[i]
+    }
+}
