66e52ee6436c40e4389069974499e8cc0882fb32
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-01-06 10:17:10 -0300

    projectDevelopment

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const titles = document.querySelector(".title-container");
+const tiles = document.querySelector(".tile-container");
+
+const backspaceAndEnterRow = document.querySelector("#backspaceAndEnterRow");
+const keyboardFirstRow = document.querySelector("#keyboardFirstRow");
+const keyboardSecondRow = document.querySelector("#keyboardSecondRow");
+const keyboardThirdRow = document.querySelector("#keyboardThirdRow");
+
+const keysFirstRow = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+const keysSecondRow = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+const keysThirdRow = ["Z", "X", "C", "V", "B", "N", "M"];
+
+
 
 const rows = 6;
 const columns = 5;
 
-for(let i; i < rows; i++) {
-    const titleRow = document.createElement("div");
-    titleRow.setAttribute("id", rowIndex);
-    titleRow.setAttribute("class", title-row);
-    for(let j = 0; j < columns; j++) {
-        const element = array[i]
+for(let rowIndex = 0; rowIndex < rows; rowIndex++) {
+    const tileRow = document.createElement("div");
+    tileRow.setAttribute("id", "row"+rowIndex);
+    tileRow.setAttribute("class", "tile-row");
+    for(let columnIndex = 0; columnIndex < columns; columnIndex++) {
+        const tileColumn = document.createElement("div");
+        tileColumn.setAttribute("id", "row"+rowIndex+"column"+columnIndex);
+        tileColumn.setAttribute("class", "tile-column");
+        tileRow.append(tileColumn);
     }
+    tiles.append(tileRow);
 }
